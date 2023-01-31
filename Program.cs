afb3fa9977edf582bb1d23b4ef9171bdcbdf2f82
Mikołaj Olszewski <123817857+MrMikx@users.noreply.github.com>
2023-01-31 22:17:53 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using AzureStorageApp.Interfaces;
+using AzureStorageApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IStorageService, AzureStorageService>();
+
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
